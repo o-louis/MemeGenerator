5991f2775d7241fb38ba636668d8211a00f1ddfd
Oriane <oriane.louis.pro@gmail.com>
2020-02-21 11:22:15 +0100

    Optimized script

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 
 function memeGenerator() {
-    var memeUrl = "https://api.imgflip.com/get_memes";
     var datas = [];
+    var memeUrl = "https://api.imgflip.com/get_memes";
     var btn = document.getElementsByTagName("button")[0];
 
     getMemes().then(json => {
         datas = json.data.memes;
         btn.addEventListener("click", function () {
             if (datas) {
-                var meme = loadRandomMeme();
-                displayMeme(meme);
-
-                var topText = document.getElementById("topText");
-                var bottomText = document.getElementById("bottomText");
-
-                var topContainer = document.getElementsByClassName("topContainer")[0];
-                var bottomContainer = document.getElementsByClassName("bottomContainer")[0];
-
-                topContainer.innerHTML = topText.value;
-                bottomContainer.innerHTML = bottomText.value;
+                var currentMeme = loadRandomMeme();
+                displayMeme(currentMeme);
             }
         });
     }).catch(error => console.error(error));
@@ -38,6 +29,15 @@ function memeGenerator() {
     function displayMeme(meme) {
         var container = document.getElementsByClassName('meme-picture')[0].getElementsByTagName("img")[0];
         container.src = meme.url;
+
+        var topText = document.getElementById("topText");
+        var bottomText = document.getElementById("bottomText");
+
+        var topContainer = document.getElementsByClassName("topContainer")[0];
+        var bottomContainer = document.getElementsByClassName("bottomContainer")[0];
+
+        topContainer.innerHTML = topText.value;
+        bottomContainer.innerHTML = bottomText.value;
     }
 }
 
